89ff09cf21406187c6101ce0b7cfe63b1a8bfd8c
geyaoAlone <gy565581868@126.com>
2018-08-30 09:49:10 +0800

    first

---
+++ src/main/java/com/yxf/pojo/Timeline.java
@@ -0,0 +1,32 @@
+package com.yxf.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Setter
+@Getter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Timeline {
+
+    private String time;
+    private String title;
+    private String content;
+    private String auhtor;
+
+    public String validate(){
+        if(title == null){
+            return "标题不能为空";
+        }
+        if(content == null){
+            return "内容不能为空";
+        }
+        return "1";
+
+    }
+
+}
